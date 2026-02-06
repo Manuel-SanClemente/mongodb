0097fa38ad0cceda83aa15878259e6e644d95ec4
a24manuelda@iessanclemente.net <a24manuelda@iessanclemente.net>
2026-02-06 09:55:39 +0100

    06/02

--- UD4-AT03/demo/src/main/java/com/example/Main.java
+++ UD4-AT03/demo/src/main/java/com/example/Main.java
@@ -5,15 +5,19 @@ public class Main {
         RecordRepo records = RecordRepo.getInstance();
 
         // Engadir records de xogadores
-        
         records.addRecord("Mario", "Space Invaders", 1200, 15, 3);
         records.addRecord("Luigi", "Galaga", 1268, 12, 4);
         records.addRecord("Arturo", "Space Invaders", 2048, 30, 5);
         records.addRecord("Mario", "Donkey Kong", 2156, 27, 4);
         records.addRecord("Anxo", "Donkey Kong", 510, 7, 2);
         records.addRecord("Luigi", "Tetris", 3946, 45, 8);
-        
-
+                
+        // Para as consultas filtradas.
         records.getTotalRecord();
+        records.getBestMatch();
+        records.getShortestMatch();
+        records.getRanking();
+        records.getSimplified();
+        records.getMostScore();
     }
 }
--- UD4-AT03/demo/src/main/java/com/example/RecordRepo.java
+++ UD4-AT03/demo/src/main/java/com/example/RecordRepo.java
@@ -1,8 +1,14 @@
 package com.example;
 
-import org.bson.Document;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Sorts;
 
 /**
  * Clase encargada do manexo dos Records
@@ -50,51 +56,109 @@ public class RecordRepo {
         }
     }
 
-
+    /**
+     * Para buscar o record total de cada xogador.
+     */
     public void getTotalRecord(){
         try (MongoProvider provider = new MongoProvider()) {
-            provider.record().find();
+            List<Document> list = new ArrayList<>();
+            List<Bson> pipeline = List.of(
+                Aggregates.group("$nome", 
+                Accumulators.sum("puntuacionTotal", "$puntuacion")),
+                Aggregates.sort(Sorts.ascending("puntuacionTotal"))
+            ); 
             
+            provider.record().aggregate(pipeline).into(list);
+            list.forEach(doc -> System.out.println(doc.toJson()));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
+    /**
+     * Para atopar a puntuación máxima de cada xogador
+     */
     public void getBestMatch(){
         try (MongoProvider provider = new MongoProvider()) {
+            List<Document> list = new ArrayList<>();
+            List<Bson> pipeline = List.of(
+                Aggregates.group("$nome", 
+                    Accumulators.max("puntuacionMaxima", "$puntuacion")),
+                    Aggregates.sort(Sorts.ascending("puntuacionMaxima"))
+            );
             
+            provider.record().aggregate(pipeline).into(list);
+            list.forEach(doc -> System.out.println(doc));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
+    /**
+     * Para obter a minima duración de partida por xogo
+     */
     public void getShortestMatch(){
         try (MongoProvider provider = new MongoProvider()) {
-            
+            List<Document> list = new ArrayList<>();
+            List<Bson> pipeline = List.of(
+                Aggregates.group("$xogo", 
+                Accumulators.min("duracion", "$duracion")
+            ));
+            provider.record().aggregate(pipeline).into(list);
+            list.forEach(doc -> System.out.println(doc));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
+    /**
+     * Para obter a clasificación dos xogadores. Obten a puntuación total, ordeada de maior a menor
+     */
     public void getRanking(){
         try (MongoProvider provider = new MongoProvider()) {
-            
+            List<Document> list = new ArrayList<>();
+            List<Bson> pipeline = List.of(
+                Aggregates.group("$nome", 
+                Accumulators.sum("puntuacionTotal", "$puntuacion")),
+                Aggregates.sort(Sorts.descending("puntuacionTotal"))
+            );
+            provider.record().aggregate(pipeline).into(list);
+            list.forEach(doc -> System.out.println(doc));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
+    /**
+     * Para obter unha consulta simplificada. Busca soamente o nome, o xogo e a puntuación, excluindo o _id na sua proxección
+     */
     public void getSimplified(){
         try (MongoProvider provider = new MongoProvider()) {
-            
+            Document projection = new Document();
+            projection.append("_id", 0);
+            projection.append("nome", 1);
+            projection.append("xogo", 1);
+            projection.append("puntuacion", 1);
+
+            provider.record().find().projection(projection).forEach(doc -> System.out.println(doc));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
+    /**
+     * Para obter a puntuación máxima que cada xogo tivo.
+     */
     public void getMostScore(){
         try (MongoProvider provider = new MongoProvider()) {
-            
+            List<Document> list = new ArrayList<>();
+            List<Bson> pipeline = List.of(
+                Aggregates.group("$xogo",
+                Accumulators.avg("puntuacionMedia", "$puntuacion")),
+                Aggregates.sort(Sorts.descending("puntuacionMedia"))
+            );
+            provider.record().aggregate(pipeline).into(list);
+            list.forEach(doc -> System.out.println(doc));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
